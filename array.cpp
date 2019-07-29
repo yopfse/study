4d49194de5631dbe495c5449d564cf1c1b613d6b
ko <ko@MacBook-Pro-3.local>
2019-07-30 00:50:51 +0900

    add new file

---
+++ array.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int cards[4][13];
+	int n, m;
+	char c;
+	
+	for(int i = 0; i < 13; i++){
+		for(int j = 0; j < 4; j++)
+			cards[j][i] = 0;
+	}
+
+	cin >> n;
+
+	for(int i = 0; i < n; i++){
+		cin >> c;
+		cin >> m;
+
+		if(c == 'S')
+			cards[0][m-1] = 1;
+
+		else if(c == 'H')
+			cards[1][m-1] = 1;
+
+		else if(c == 'C')
+			cards[2][m-1] = 1;
+
+		else
+			cards[3][m-1] = 1;
+	}
+
+	for(int i = 0; i < 4; i++){
+		for(int j = 0; j < 13; j++){
+			if(cards[i][j] == 0){
+				if(i == 0)
+					cout << "S " << j+1 << endl;
+
+				else if(i == 1)
+					cout << "H " << j+1 << endl;
+
+				else if(i == 2)
+					cout << "C " << j+1 << endl;
+
+				else 
+					cout << "D " << j+1 << endl;
+
+			}
+		}
+	}
+}
+		
