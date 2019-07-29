8a5d3560493d48ca6aea3e4fb9e02b08e860fe7c
ko <ko@MacBook-Pro-3.local>
2019-07-30 01:25:05 +0900

    add new file

---
+++ structured.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,count = 0,a;
+	cin >> n;
+
+	for(int i = 1; i <= n; i++){
+	      
+		if(i%3 == 0 || i%10 == 3 || i/10 == 3){
+			cout << " ";
+			cout << i;
+		}
+		
+		else{
+			a = i;
+			while(a > 0){
+				a /= 10;
+
+				if(a%10 == 3){
+					cout << " ";
+					cout <<	i;
+					break;
+				}
+			}
+		}
+	}
+	cout << endl;
+}
+
+
