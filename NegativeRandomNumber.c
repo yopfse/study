5a43858bee46bad50fd5dc0b37050e52bea8ec40
ko <ko@MacBook-Pro-3.local>
2019-07-30 15:34:07 +0900

    add new file

---
+++ NegativeRandomNumber.c
@@ -0,0 +1,16 @@
+/* This program is which 10 integer random numbers in the range of -10 or more and 10 or less */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+	int i;
+
+	printf("Generate random number\n");
+
+	for(i = 0; i < 10; i++)
+		printf("%d\n", rand()%21-10);
+
+	return 0;
+}
